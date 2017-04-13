1c9c3df35e8ab387bc783881b13079f99ca2b5aa
Krystian Lema <krystian.lema@gmail.com>
2017-04-13 23:19:26 +0200

    Unique username validation. Delete user test.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,19 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
 	test "valid with all attributes" do
-		u = User.new
-		u.username = 'NewUser'
-		u.email = 'newuser@email.com'
-		u.password = 'password'
-		u.password_confirmation = 'password'
-		assert u.valid?, "User was not valid"
-	end
-
-	test "check user" do
 		u = users(:one)
 		assert u.valid?
 	end
-
-
 
 end
